08c11e2c38f478cddc1e74ce6b3025acb3273fb3
yijianyufan <yijianyufan@outlook.com>
2016-10-19 15:25:53 +0800

    haha

---
+++ oneproject/oneproject/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("oneproject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("oneproject")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7451d36d-0418-4309-a71b-02b0ee11052d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ oneproject/oneproject/scripts/perloadimg.js
@@ -0,0 +1,9 @@
+﻿function preloadimages(arrs) {
+    var newimages = [];
+    var arr = arrs.split(" ");
+    for (var i = 0; i < arr.length; i++) {
+        newimages[i] = new Image();
+        newimages[i].src = "../images/" + arr[i];
+    }
+}
+preloadimages("topbanner.jpg index1.jpg index2.jpg index3.jpg index4.jpg  bg1.jpg bg2.jpg bg3.jpg bg4.jpg  info.jpg bottom.jpg aboutflash.jpg fwcenter.jpg wsmm.jpg verone.jpg aboutinfo.jpg contact.jpg");
